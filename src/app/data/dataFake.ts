79d38b2a19bd33610c2864819e34408b6316547f
Juan Carlos Leone Barreto <jc.leone98@gmail.com>
2024-02-07 16:58:57 -0300

    feat: content Page Dinamica and memori database

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo": "aaa",
-    "title": "titulo",
-    "description": "descrição"
+    "id": "1",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQCyXga0LgtUES34EepPai9hU5iGpcQdHy7Q&usqp=CAU",
+    "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+    "description": "Novo homem de ferro será lançado em 2025, com a filha sendo a protagonista"
   },
   {
-    "id": 2,
-    "photo": "aaa",
-    "title": "titulo2",
-    "description": "descrição2"
+    "id": "2",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6xXUss6813GI7RGo89kxdzKQHiwXPGmlgrw&usqp=CAU",
+    "title": "HOMEM DE FERRO MORRE EM BATALHA CONTRA THANOS",
+    "description": "Novo homem de ferro será lançado em 2025, com a filha sendo a protagonista"
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -10,14 +11,26 @@ export class ContentComponent implements OnInit {
   photoCover: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQCyXga0LgtUES34EepPai9hU5iGpcQdHy7Q&usqp=CAU"
   contentTitle:string = "HOMEM DE FERRO MORRE EM BATALHA CONTRA THANOS"
   contentDescription: string = "Novo homem de ferro será lançado em 2025, com a filha sendo a protagonista"
+  private id:string | null = '0'
   constructor(
     private route:ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id = value.get("id")
     );
+
+    this.setValuesComponent(this.id)
+  }
+
+  setValuesComponent(id:string | null) {
+    const result = dataFake.filter(
+                    article => article.id.toString() === id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
   }
 
 }
